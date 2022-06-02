5ef93e7f6852bd7e09d613fa9fe37f3128b29cfb
Michelle Caraballo <caraballomp@yahoo.com>
2022-06-01 21:23:05 -0500

    upates styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ def trash_em
     @trash_em = List.where(:completed => true).destroy_all
     redirect_to lists_url(@list), notice: "Marked items were successfully deleted." 
 end
+
+def trash_em_all
+    @trash_em_all = List.all.destroy_all
+    redirect_to lists_url(@list), notice: "All items were successfully deleted." 
+end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'lists#index'
   get 'home/trash_em'
+  get 'home/trash_em_all'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
